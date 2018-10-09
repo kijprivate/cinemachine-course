b232025dacb4651c7776743a4d1197a9549a7491
DESKTOP-2T6H5L0\Kuba <brodaczz1@gmail.com>
2018-10-09 16:55:19 +0200

    Added navmesh and agent to move player capsule

---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    private NavMeshAgent navMeshAgent;
+	// Use this for initialization
+	void Start ()
+	{
+	    navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if(Physics.Raycast(rayOrigin,out hitInfo))
+            {
+                Debug.Log("Hit: " + hitInfo.point);
+
+                navMeshAgent.destination = hitInfo.point;
+                // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                // cube.transform.position = hitInfo.point;
+            }
+        }
+	}
+}
