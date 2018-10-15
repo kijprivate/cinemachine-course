9b611ec7da948df2cef94a9025a94ec6d38d4f97
DESKTOP-2T6H5L0\Kuba <brodaczz1@gmail.com>
2018-10-15 10:11:06 +0200

    Finished sleeping guard cutscene activation

---
+++ Assets/GrabKeyCardActivate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabKeyCardActivate : MonoBehaviour
+{
+
+    public GameObject SleepingGuardCutscene;
+
+    private bool wasPlayed = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !wasPlayed)
+        {
+            SleepingGuardCutscene.SetActive(true);
+            wasPlayed = true;
+        }
+    }
+}
